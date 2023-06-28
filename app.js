4d31413ac35a508fadd22f52bece7732b5f1a7c7
Paramak <lakeza551@gmail.com>
2023-06-28 15:43:44 +0700

    add su login

--- app.js
+++ app.js
@@ -1,13 +1,67 @@
 const express = require('express')
 const path = require('path')
+const queryString = require('querystring')
+const https = require('follow-redirects').https
 const app = express()
 
 const PORT = 3000
 
+const CLIENT_ID = '1d5d0546-0f74-400a-9bad-22182c72de3d'
+const CLIENT_SECRET = 'KrKCyu9W5oC90kgZEx-kbUQfr4lFffj0UXxE8ACtB1TwRQzqotq-g088X5vjlCFj0aVppE-mkAwlT78yxHtHAA'
+
 app.use(express.static('build'))
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.redirect(
+        `https://nidp.su.ac.th/nidp/oauth/nam/authz?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&response_type=code&grant_type=authorization_code&scope=suappportal&redirect_uri=http://workload.sc.su.ac.th/su-auth`
+    )
+})
+
+app.get('/redirect', async(req, res) => {
+    const { code } = req.query
+    const data = queryString.stringify({
+        'code': code,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'redirect_uri': `http://workload.sc.su.ac.th/su-auth`
+    })
+    const options = {
+        hostname: 'nidp.su.ac.th',
+        port: 443,
+        path: "/nidp/oauth/nam/token",
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': data.length
+          }
+      }
+
+    var request = https.request(options, function(response) {
+        var chunks = [];
+
+        response.on("data", function(chunk) {
+            chunks.push(chunk);
+        });
+
+        response.on("end", function(chunk) {
+            var body = JSON.parse(Buffer.concat(chunks).toString());
+            const { access_token } = body
+            console.log(body)
+            //console.log(body.toString());
+        });
+
+        response.on("error", function(error) {
+            res.send(error)
+            console.error(error);
+        });
+    });
+    request.write(data)
+    request.end()
 })
 
 app.listen(PORT, () => {
--- src/Auth.jsx
+++ src/Auth.jsx
@@ -8,40 +8,13 @@ function Auth() {
     const [password, setPassword] = useState('')
     const cookies = new Cookies()
     const navigate = useNavigate()
-    const login = async () => {
-        const raw = JSON.stringify({
-            username: username,
-            password: password
-        })
-        const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/auth/basic_login/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: raw
-        })
-        const resData = await res.json()
-        if (resData.status === 'success') {
-            cookies.set(process.env.REACT_APP_COOKIE_NAME_TOKEN, resData.data, {
-                path: '/'
-            })
-            return navigate('/')
-        }
-        alert('ไม่สามารถเข้าสูระบบได้')
-    }
 
     return (
         <div className="homepage-container">
             <div className="homepage-card">
                 <label className="homepage-header">SC-Workload</label>
                 <div className="homepage-login-form">
-                    <input type="text" placeholder="Username" onChange={e => {
-                        setUsername(e.target.value)
-                    }} />
-                    <input type="password" placeholder="Password" onChange={e => {
-                        setPassword(e.target.value)
-                    }} />
-                    <button onClick={login} className="homepage-button">เข้าสู่ระบบ</button>
+                    <a href="/login" className="homepage-button">เข้าสู่ระบบ</a>
                 </div>
             </div>
         </div>
