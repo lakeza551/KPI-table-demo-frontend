fc321f77d14612e9a6d0f810f392ef73a088895c
Paramak <lakeza551@gmail.com>
2023-07-05 13:45:48 +0000

    change title

--- services/hashPassword.js
+++ services/hashPassword.js
@@ -5,6 +5,7 @@ dotenv.config()
 const hashPassword = password => {
     const {PASSWORD_SALT} = process.env
     const hashedPassword = crypto.createHash('md5').update(PASSWORD_SALT + password + PASSWORD_SALT).digest('hex')
+    console.log(hashedPassword)
     return hashedPassword
 }
 
